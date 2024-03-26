2eb4c02550bddd511d00b0f2e955100dd69687bc
Leonardo Viana <leorocha11@outlook.com>
2024-03-26 16:31:15 -0300

    navbar 2.0

--- js/navbar.js
+++ js/navbar.js
@@ -9,3 +9,15 @@ document.addEventListener("DOMContentLoaded", function () {
       navbar.innerHTML = data;
     });
 });
+
+function menuShow() {
+  let menuMobile = document.querySelector(".mobile-menu");
+
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+    document.querySelector(".icon").src = "./src/assets/image/cardapio.png";
+  } else {
+    menuMobile.classList.add("open");
+    document.querySelector(".icon").src = "./src/assets/image/perto.png";
+  }
+}
